0beb22f4bf89ecdab130eeb54c1c5957d8ab44ae
Dayo Banjo <banjodayo39@gmail.com>
2024-06-25 20:10:42 -0700

    Change package name

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "BBMetalImage",
-    platforms: [.iOS(.v10)],
+    name: "MetalPexel",
+    platforms: [.iOS("13.0")],
     products: [
         .library(name: "BBMetalImage", targets: ["BBMetalImage"]),
     ],
